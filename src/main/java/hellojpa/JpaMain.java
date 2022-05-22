5c77bb089ccf364db6e37cc13f65fbabfd1c538e
LeeJuOh <xop4p@naver.com>
2022-05-22 22:43:04 +0900

    feat : add jpa proxy

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,10 +14,29 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = em.find(Member.class, 1L);
+            Member member1 = new Member();
+            member1.setUsername("m1");
+            em.persist(member1);
+            Member member2 = new Member();
+            member2.setUsername("m2");
+            em.persist(member2);
+            em.flush();
+            em.clear();
 
-            em.detach(member);
+            Member m1 = em.find(Member.class, member1.getId());
+            Member m2 = em.find(Member.class, member2.getId());
+            System.out.println(m1.getClass());
+            System.out.println(m2.getClass());
+            System.out.println(m1.getClass() == m2.getClass());
             System.out.println("=========");
+            em.clear();
+            Member proxyM1 = em.getReference(Member.class, member1.getId());
+            Member proxyM2 = em.getReference(Member.class, member2.getId());
+            System.out.println(proxyM1.getClass());
+            System.out.println(proxyM2.getClass());
+            System.out.println(proxyM1.getClass() == proxyM2.getClass());
+            System.out.println(proxyM1 instanceof Member);
+            System.out.println(proxyM2 instanceof Member);
             tx.commit();
         } catch (Exception exception) {
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -5,6 +5,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.Table;
@@ -16,6 +18,7 @@ import javax.persistence.TemporalType;
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(name = "name")
@@ -34,4 +37,60 @@ public class Member {
 
     @Lob
     private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
